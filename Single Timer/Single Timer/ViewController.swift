79a589fd0b6c6ed942a80f870942fa7c6fa53116
turtlexuan <turtlexuan@gmail.com>
2017-03-20 17:33:42 +0800

    1. Add project 2. Add gitignore.

---
+++ Single Timer/Single Timer/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Single Timer
+//
+//  Created by 劉仲軒 on 2017/3/20.
+//  Copyright © 2017年 劉仲軒. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func startAction(_ sender: Any) {
+    }
+    
+    @IBAction func pauseAction(_ sender: Any) {
+    }
+
+    @IBAction func resetAction(_ sender: Any) {
+    }
+}
+
